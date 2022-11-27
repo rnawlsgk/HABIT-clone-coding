d1415ffa01e75524f4404f15bceffb1a26a12d8b
rnawslgk <hamo35301@gmail.com>
2022-11-27 13:04:12 +0900

    bottomNavigationView

--- app/src/main/java/com/example/clone_coding_habit/MainActivity.java
+++ app/src/main/java/com/example/clone_coding_habit/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.clone_coding_habit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.clone_coding_habit.fragment.HomeFragment;
+import com.example.clone_coding_habit.fragment.RecordFragment;
+import com.example.clone_coding_habit.fragment.SearchFragment;
+import com.example.clone_coding_habit.fragment.SettingFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        getSupportFragmentManager().beginTransaction().add(R.id.linearLayout, new HomeFragment()).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                switch (item.getItemId()) {
+                    case R.id.page_home:
+                        transaction.replace(R.id.linearLayout, new HomeFragment()).commitAllowingStateLoss();
+                        break;
+                    case R.id.page_calender:
+                        transaction.replace(R.id.linearLayout, new RecordFragment()).commitAllowingStateLoss();
+                        break;
+                    case R.id.page_search:
+                        transaction.replace(R.id.linearLayout, new SearchFragment()).commitAllowingStateLoss();
+                        break;
+                    case R.id.page_setting:
+                        transaction.replace(R.id.linearLayout, new SettingFragment()).commitAllowingStateLoss();
+                        break;
+                }
+
+                return true;
+            }
+        });
     }
 }
---
+++ readMe.md
@@ -0,0 +1,3 @@
+bottomNavigationView
+ - [유연한 UI 빌드](https://developer.android.com/training/basics/fragments/fragment-ui?hl=ko#java)
+ - [bottomNavigationView kt](https://4z7l.github.io/2020/07/09/bottomnavigationbar.html)
